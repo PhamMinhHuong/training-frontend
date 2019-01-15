25b8ea75c4f16f26d079a4ce11528d94a3b37e64
PhamMinhHuong <huongpm5991@gmail.com>
2019-01-15 14:29:42 +0700

    Init source code

---
+++ green-shop/source/js/main.js
@@ -0,0 +1,12 @@
+//=require node_modules/jquery/dist/jquery.min.js
+//=require node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
+//=require _test.js
+
+const GreenProject = {
+  Init: function () {
+  },
+},
+
+$(document).ready(function () {
+  GreenProject.Init();
+});
