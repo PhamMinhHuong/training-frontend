32ec00ac1fb3f4b22307fd64996a6dd53ce608fa
cotrieuphongfr <co.trieu.phong@framgia.com>
2019-01-21 08:24:25 +0700

    home page

--- green-shop/source/js/main.js
+++ green-shop/source/js/main.js
@@ -1,12 +1,26 @@
 //=require node_modules/jquery/dist/jquery.min.js
 //=require node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
 //=require _test.js
+//=require node_modules/slick-carousel/slick/slick.min.js
 
-const GreenProject = {
-  Init: function () {
-  },
-},
+$(function(){
 
-$(document).ready(function () {
-  GreenProject.Init();
-});
+  $('.menu, .menu__overlay').click(function(){
+    $('.menu__overlay, .menu__content').toggleClass('active');
+  });
+
+  $('.header__expand-search').hide();
+  $('.expand__search').click(function(){
+    $('.header__expand-search').show();
+  })
+  $('.expand-search__btn').click(function(){
+    $('.header__expand-search').hide();
+  })
+  $('.slider').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    arrows: false
+  });
+})
